f417f55b421303756912d7ad97ad877115aa25f9
alibekov55 <93965749+alibekov55@users.noreply.github.com>
2022-03-15 00:26:56 +0600

    1

--- project/news/urls.py
+++ project/news/urls.py
@@ -4,7 +4,7 @@ from .views import NewsList, PostDetailView, PostCreateView, PostUpdateView, Pos
 
 #from django.views.decorators.cache import cache_page
 urlpatterns = [# path означает "путь". В данном случае путь ко всем товарам у нас останется пустым, позже станет ясно, почему
-    path('', NewsList.as_view()), # т. к. сам по себе это класс, то нам надо представить этот класс в виде view. Для этого вызываем метод as_view
+    path('', NewsList.as_view(), name="news_list"), # т. к. сам по себе это класс, то нам надо представить этот класс в виде view. Для этого вызываем метод as_view
     path('<int:pk>', (PostDetailView.as_view()), name='post_detail'), #cache_page(100) # pk — это первичный ключ товара, который будет выводиться у нас в шаблон
     path('create/', PostCreateView.as_view(), name='post_create'),
     path('update/<int:pk>', PostUpdateView.as_view(), name='post_update'),
--- project/project/urls.py
+++ project/project/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import IndexView
 
 urlpatterns = [
+    path('', IndexView.as_view()),
     path('admin/', admin.site.urls),
     path('pages/', include('django.contrib.flatpages.urls')),
     path('products/', include('simpleapp.urls')),  # делаем так, чтобы все адреса из нашего приложения (simpleapp/urls.py) сами автоматически подключались, когда мы их добавим.
